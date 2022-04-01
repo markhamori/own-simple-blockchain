2c3072c9481e0131f2347ed932642c05e00e4223
manqn <hamorimark@hotmail.com>
2022-04-01 20:15:30 +0200

    Rewards and transactions

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-# Create own blockhain #
+# Create own blockhain
 
 **Using Proof-of-Work mechanism**
-*(We need to put a lot of computing power into making a block - this process is also call mining)*
+_(We need to put a lot of computing power into making a block - this process is also call mining)_
 
 **Security issues without PoW:**
+
 - Anyone can change the contents of the block
 - Recalculate the hashes all for all the blocks
-- It ends up with a valid chain 
+- It ends up with a valid chain
+
+**Mining rewards & transactions:**
+
+- In Bitcoin's case, PoW algorithm make sura that there's only one block created every 10 minutes
+- All the transactions that are made in between blocks are temporarily stored in this pendingTransactions array
+- They can be included in the next block
--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const SHA256 = require("crypto-js/sha256");
 
+// Create transactions class
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+
 class Block {
-  constructor(index, timestamp, data, previousHash = "") {
-    this.index = index;
+  constructor(timestamp, transactions, previousHash = "") {
     this.timestamp = timestamp;
-    this.data = data;
+    // Modify the block to supports multiple transactions
+    this.transactions = transactions;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
     this.nonce = 0;
@@ -37,21 +46,50 @@ class Block {
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
-    this.difficulty = 4;
+    this.difficulty = 2;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
   }
 
   createGenesisBlock() {
-    return new Block(0, "22/02/2022", "Genesis block", "0");
+    return new Block("22/02/2022", "Genesis block", "0");
   }
 
   getLatesBlock() {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(newBlock) {
-    newBlock.previousHash = this.getLatesBlock().hash;
-    newBlock.mineBlock(this.difficulty);
-    this.chain.push(newBlock);
+  minePendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions);
+    block.mineBlock(this.difficulty);
+
+    console.log("Block successfully mined!");
+    this.chain.push(block);
+
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward),
+    ];
+  }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAddress === address) {
+          balance -= trans.amount;
+        }
+
+        if (trans.toAddress === address) {
+          balance += trans.amount;
+        }
+      }
+    }
+
+    return balance;
   }
 
   // Adding validation functionality
@@ -75,18 +113,21 @@ class Blockchain {
 
 let HMARTXCoin = new Blockchain();
 
-console.log("Mining block 1...");
-HMARTXCoin.addBlock(new Block(1, "01/04/2022", { amount: 4 }));
-console.log("Mining block 2...");
-HMARTXCoin.addBlock(new Block(2, "12/04/2022", { amount: 10 }));
+HMARTXCoin.createTransaction(new Transaction("address1", "address2", 100));
+HMARTXCoin.createTransaction(new Transaction("address2", "address1", 50));
 
-// Verify the integrity of our blockchain
-// console.log("Is blockchain valid? " + HMARTXCoin.isChainValid());
+console.log("\n Starting the miner...");
+HMARTXCoin.minePendingTransactions("hmartx-address");
 
-// console.log(JSON.stringify(HMARTXCoin, null, 4));
+console.log(
+  "\nBalance of hmartx is",
+  HMARTXCoin.getBalanceOfAddress("hmartx-address")
+);
 
-// Temper the block chain
-// HMARTXCoin.chain[1].data = { amount: 100 };
-// HMARTXCoin.chain[1].hash = HMARTXCoin.chain[1].calculateHash();
+console.log("\n Starting the miner again...");
+HMARTXCoin.minePendingTransactions("hmartx-address");
 
-// console.log("Is blockchain valid?" + HMARTXCoin.isChainValid());
+console.log(
+  "\nBalance of hmartx is",
+  HMARTXCoin.getBalanceOfAddress("hmartx-address")
+);
